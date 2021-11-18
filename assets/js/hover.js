5a84d9d6980d896b28371d30d757720dfc8ac4f3
= <=>
2021-11-18 10:23:05 +0100

    javascript media query navigation animation

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -1,4 +1,9 @@
 function showDisplay (e){
+    // console.log(e);
+    if (!window.matchMedia("(max-width: 22rem) and (orientation: portrait)").matches) {
+        // console.log('media query disabled effect');
+        return ;
+    }
     let span = e.children[1];
     span.style.display="inline-block";
     setTimeout(() => {
@@ -7,10 +12,37 @@ function showDisplay (e){
     return ;
 }
 function hideDisplay (e){
+    // console.log(e);
+    if (!window.matchMedia("(max-width: 22rem) and (orientation: portrait)").matches) {
+        // console.log('media query disabled effect');
+        return ;
+    }
     let span = e.children[1];
     span.style.opacity="0";    
     setTimeout(() => {
         span.style.display="none";    
     }, 500);
     return ;
+}
+function changeMediaQ () {
+    let mq = window.matchMedia("(max-width: 22rem) and (orientation: portrait)");
+    mq.onchange = (e) => {
+        // console.log('mq.onchange event');
+        if(!e.matches) {
+            let checks = document.querySelectorAll('.check');
+            checks.forEach(element => {
+                element.children[1].style.opacity="1";
+                element.children[1].style.display="inline-block";
+    
+            });
+        } else {
+            let checks = document.querySelectorAll('.check');
+            checks.forEach(element => {
+                element.children[1].style.opacity="0";
+                element.children[1].style.display="none";
+    
+            });
+        }
+    }
+    
 }
